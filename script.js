3ee25245cd17bc80808abb2f4fa6ef96a264da86
Harsh Raj <128452067+harshraj2008@users.noreply.github.com>
2024-10-29 11:40:42 +0530

    made website responsive

--- script.js
+++ script.js
@@ -67,3 +67,8 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('active');
+}
